31c588958e9835d197b68f0952be2df4cb421468
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-06-06 02:07:01 -0500

    things

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Weather from "./Weather"
 function App() {
   return (
     <div className="App">
-      <Weather/ >
+      <Weather />
+      <small>This website was coded by <a href="https://www.instagram.com/sofiahinojosadl_/" target="_blank" rel="noreferrer">Sofia Hinojosa</a> and is open-sourced on <a href="https://github.com/sofiahinojosa/react-weather-project" target="_blank" rel="noreferrer">GitHub</a></small>
     </div>
   );
 }
